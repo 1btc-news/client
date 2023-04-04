67b3ba2301d5b72b9dac4331cc1f93d3c844b506
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-04 15:35:17 -0700

    fix: update recent news feed and style

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     element: <ViewNews />,
   },
   {
-    path: '/recent-news',
+    path: '/newest',
     element: <RecentNews />,
   },
 ]);
--- src/routes/recent-news.tsx
+++ src/routes/recent-news.tsx
@@ -1,63 +1,64 @@
 import { useEffect, useState } from 'react';
-import { Heading, HStack, Text } from '@chakra-ui/react';
-import { KVNamespaceListKey } from '@cloudflare/workers-types';
+import { Box, Divider, Heading, HStack, Text, VStack } from '@chakra-ui/react';
+import { KVNamespaceListKey, KVNamespaceListResult } from '@cloudflare/workers-types';
 import { InscriptionMeta, OrdinalNews } from '../../lib/api-types';
 import { Link } from 'react-router-dom';
-import StatsCard from '../components/stats-card';
 
 const apiUrl = new URL('https://inscribe.news/');
 
 async function getRecentNews() {
-  const list = await fetch(new URL(`/api/data/ord-news`, apiUrl).toString());
-  if (list.ok) {
-    // TODO: type as KVNamespaceListKey[]
-    const infoData: KVNamespaceListKey<unknown, string>[] = await list.json();
+  const result = await fetch(new URL(`/api/data/ord-news`, apiUrl).toString());
+  if (result.ok) {
+    const infoData: KVNamespaceListResult<unknown, string> = await result.json();
     return infoData;
   }
-  console.log(`getRecentNews: ${list.status}`);
+  console.log(`getRecentNews: ${result.status} ${result.statusText}`);
   return undefined;
 }
 
 async function getNewsData(id: string) {
   const news = await fetch(new URL(`/api/data/${id}`, apiUrl).toString());
   if (news.ok) {
-    console.log(`news: ${typeof news}`);
-    console.log(`news: ${JSON.stringify(news)}`);
+    //console.log(`news: ${typeof news}`);
+    //console.log(`news: ${JSON.stringify(news)}`);
     const newsData: InscriptionMeta & OrdinalNews = await news.json();
     return newsData;
   }
-  console.log(`getNewsData: ${news.status}`);
+  console.log(`getNewsData: ${news.status} ${news.statusText}`);
   return undefined;
 }
 
 function NewsItem(props: InscriptionMeta & OrdinalNews) {
   const { id, number, timestamp, title, author } = props;
   return (
-    <HStack
-      my={3}
-      alignItems="stretch"
+    <VStack
+      alignItems="flex-start"
+      w="100%"
+      maxW={1200}
+      py={4}
     >
-      <StatsCard
-        title={undefined}
-        stat={<Link to={`/view-news?id=${id}`}>{title}</Link>}
-      />
-      <StatsCard
-        title="News #"
-        stat="coming soon"
-      />
-      <StatsCard
-        title="Inscription #"
-        stat={number.toLocaleString()}
-      />
-      <StatsCard
-        title="Author"
-        stat={author ? author : 'anonymous'}
-      />
-      <StatsCard
-        title="Timestamp"
-        stat={new Date(timestamp).toLocaleString()}
-      />
-    </HStack>
+      <Heading
+        size="md"
+        textAlign="left"
+      >
+        <Link
+          className="link-wrap-hack"
+          to={`/view-news?id=${number}`}
+        >
+          {title}
+        </Link>
+      </Heading>
+      <HStack flexWrap="wrap">
+        <Text>{new Date(timestamp).toLocaleString()}</Text>
+        <Text>•</Text>
+        <Text>News # 005</Text>
+        <Text>•</Text>
+        <Text>Inscription # {number.toLocaleString()}</Text>
+        <Text>•</Text>
+        <Text>{author ? author : 'anonymous'}</Text>
+      </HStack>
+      <Divider />
+    </VStack>
   );
 }
 
@@ -72,7 +73,8 @@ export default function RecentNews() {
     getRecentNews()
       .then(data => {
         if (data) {
-          setNewsList(data.map((key: any) => key.name));
+          const newsList = data.keys.map((key: KVNamespaceListKey<unknown, string>) => key.name);
+          setNewsList(newsList);
         }
       })
       .catch(err => {
@@ -83,36 +85,79 @@ export default function RecentNews() {
 
   useEffect(() => {
     if (newsList && newsList.length > 0) {
+      const processedNewsIds = new Set();
       for (const newsId of newsList) {
-        getNewsData(newsId)
-          .then(data => {
-            if (data) {
-              setNewsData(prev => {
-                if (prev) {
-                  return [...prev, data];
-                }
-                return [data];
-              });
-            }
-          })
-          .catch(err => {
-            console.log(`getNewsData: ${err}`);
-          });
+        if (!processedNewsIds.has(newsId)) {
+          processedNewsIds.add(newsId);
+          getNewsData(newsId)
+            .then(data => {
+              if (data) {
+                setNewsData(prev => {
+                  if (prev) {
+                    return [...prev, data];
+                  }
+                  return [data];
+                });
+              }
+              setLoading(false);
+            })
+            .catch(err => {
+              console.log(`getNewsData: ${err}`);
+              setLoading(false);
+            });
+        }
       }
-      setLoading(false);
     }
   }, [newsList]);
 
   if (loading) {
-    return <Text>Loading...</Text>;
+    return (
+      <Box
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="left"
+        w="100%"
+        minH="100vh"
+        py={8}
+        px={4}
+      >
+        <Text>Loading...</Text>
+      </Box>
+    );
   }
 
   if (!newsList || !newsData) {
-    return <Text>Failed to load news.</Text>;
+    return (
+      <Box
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="left"
+        w="100%"
+        minH="100vh"
+        py={8}
+        px={4}
+      >
+        <Text>Failed to load news.</Text>
+      </Box>
+    );
   }
 
   return (
-    <>
+    <Box
+      display="flex"
+      flexDir="column"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="left"
+      w="100%"
+      minH="100vh"
+      py={8}
+      px={4}
+    >
       <Heading>News Feed</Heading>
       <Text>
         Detected {newsList.length} news inscription{newsList.length > 1 ? 's' : null}
@@ -129,6 +174,6 @@ export default function RecentNews() {
             {...news}
           />
         ))}
-    </>
+    </Box>
   );
 }
