8e106700fa20407931d3b19f2f598844fd60a62c
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-05 12:29:13 -0700

    fix: change location of HelmetProvider

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Root404 from './routes/root-404';
 import PostNews from './routes/post-news';
 import ViewNews from './routes/view-news';
 import RecentNews from './routes/recent-news';
-import { HelmetProvider } from 'react-helmet-async';
 
 const router = createBrowserRouter([
   {
@@ -28,11 +27,9 @@ const router = createBrowserRouter([
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <ChakraProvider theme={theme}>
-        <Fonts />
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </HelmetProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
