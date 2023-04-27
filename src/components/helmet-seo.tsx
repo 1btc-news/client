59f3e209c57cd0789f4f9db94ce0fad502270a2c
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-27 14:01:18 -0700

    fix: update url from staging to production

--- src/components/helmet-seo.tsx
+++ src/components/helmet-seo.tsx
@@ -10,8 +10,8 @@ interface SeoProps {
 export default function HelmetSeo(props: SeoProps) {
   const title = `${props.title} | 1btc.news` ?? '1btc.news';
   const description = props.description ?? 'News on the Ledger of Record.';
-  const image = props.image ?? 'https://client-2zi.pages.dev/logos/1btc-news-orange.png';
-  const url = props.url ?? 'https://client-2zi.pages.dev/';
+  const image = props.image ?? 'https://1btc.news/logos/1btc-news-orange.png';
+  const url = props.url ?? 'https://1btc.news/';
 
   return (
     <Helmet>
