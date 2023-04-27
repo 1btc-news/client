4ed847d479cfc3dedf0871558b5e056b82d7348d
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-27 14:56:50 -0700

    fix: add more footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,58 @@
-import { HStack, Text } from '@chakra-ui/react';
+import {
+  HStack,
+  Link as ChakraLink,
+  Text,
+  Icon,
+  VStack,
+  Heading,
+  Stack,
+  Divider,
+} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import BitcoinIcon from './bitcoin-icon';
 
 export default function Footer() {
   return (
-    <HStack
-      w="100%"
-      maxW={1200}
-      justifyContent="space-between"
+    <VStack
+      fontWeight={900}
+      alignItems="start"
     >
-      <HStack fontWeight={900}>
-        <Link to="/">Home</Link>
-      </HStack>
-      <Text
-        as="i"
-        fontSize={['md', 'lg']}
-        color={'gray.400'}
-        fontWeight={900}
-      >
+      <Heading pt={6}>
         News on the Ledger of Record <BitcoinIcon color="#F7931A"></BitcoinIcon>
-      </Text>
-    </HStack>
+      </Heading>
+      <Stack direction={['column', 'column', 'row']}>
+        <Link to="/">1btc.news Home</Link>
+        <Divider
+          orientation="vertical"
+          hideBelow="sm"
+        />
+        <ChakraLink
+          isExternal
+          href="https://github.com/1btc-news/client"
+        >
+          1btc.news GitHub
+        </ChakraLink>
+        <Divider
+          orientation="vertical"
+          hideBelow="sm"
+        />
+        <ChakraLink
+          isExternal
+          href="https://docs.inscribe.news"
+        >
+          Ordinal News Standard
+        </ChakraLink>
+        <Divider
+          orientation="vertical"
+          hideBelow="sm"
+        />
+        <ChakraLink
+          isExternal
+          href="https://inscribe.news/post-news"
+        >
+          Inscribe the News
+        </ChakraLink>
+      </Stack>
+    </VStack>
   );
 }
