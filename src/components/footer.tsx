5b959280ed6257134dcf9350285176c79c74575f
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-04-28 07:00:30 -0700

    fix: make footer aware of location
    If it's on the home page, just show the inscribe link, otherwise show a home link too.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,10 @@
-import {
-  HStack,
-  Link as ChakraLink,
-  Text,
-  Icon,
-  VStack,
-  Heading,
-  Stack,
-  Divider,
-} from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link as ChakraLink, VStack, Heading, Stack, Divider } from '@chakra-ui/react';
+import { Link, useLocation } from 'react-router-dom';
 import BitcoinIcon from './bitcoin-icon';
 
 export default function Footer() {
+  const location = useLocation();
+
   return (
     <VStack
       fontWeight={900}
@@ -23,11 +16,15 @@ export default function Footer() {
         News on the ledger of record <BitcoinIcon color="#F7931A"></BitcoinIcon>
       </Heading>
       <Stack direction={['column', 'column', 'row']}>
-        <Link to="/">1btc.news home</Link>
-        <Divider
-          orientation="vertical"
-          hideBelow="sm"
-        />
+        {location.pathname !== '/' && (
+          <>
+            <Link to="/">1btc.news home</Link>
+            <Divider
+              orientation="vertical"
+              hideBelow="sm"
+            />
+          </>
+        )}
         <ChakraLink
           isExternal
           href="https://docs.inscribe.news"
